db2a676628f1319acfbc4eee5307bf5b55c05fde
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2023-10-19 03:59:12 +0100

    Lint fix

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { ApplicationCommandOptionType } from "replugged/dist/types";
 
 const inject = new Injector();
 
-const LINK_BOUNDARIES = " ()[]"
+const LINK_BOUNDARIES = " ()[]";
 
 export function start(): void {
   inject.utils.registerSlashCommand({
@@ -18,49 +18,52 @@ export function start(): void {
       },
     ],
     executor: (i) => {
-      const inp = i.getValue("message")
+      const inp = i.getValue("message");
 
       let isInLink = false,
-          shouldCap = false
+        shouldCap = false;
 
       return {
-        result: inp.split("").map((v, i) => {
+        result: inp
+          .split("")
+          .map((v, i) => {
             if (LINK_BOUNDARIES.includes(v)) {
-              let start = inp.slice(i + 1)
-              isInLink = start.startsWith("http://") || start.startsWith("https://")
+              let start = inp.slice(i + 1);
+              isInLink = start.startsWith("http://") || start.startsWith("https://");
               if (isInLink) {
-                shouldCap = false
+                shouldCap = false;
               }
 
-              return v
+              return v;
             }
-    
+
             if (isInLink) {
-              return v
+              return v;
             }
-    
-            let genOG =  String.prototype.toUpperCase,
-                genNew = String.prototype.toLowerCase
-    
+
+            let genOG = String.prototype.toUpperCase,
+              genNew = String.prototype.toLowerCase;
+
             if (shouldCap) {
-                genOG = String.prototype.toLowerCase
-                genNew = String.prototype.toUpperCase
+              genOG = String.prototype.toLowerCase;
+              genNew = String.prototype.toUpperCase;
             }
-    
-            let vOg = genOG.call(v)
-            let vNew = genNew.call(v)
-            
+
+            let vOg = genOG.call(v);
+            let vNew = genNew.call(v);
+
             // Don't skip a cap for syntax stuff like [ or *
             if (vOg != vNew) {
-                shouldCap = !shouldCap
+              shouldCap = !shouldCap;
             }
-            
-            return vNew
-        }).join(""),
-        send: true
-      }
-    }
-  })
+
+            return vNew;
+          })
+          .join(""),
+        send: true,
+      };
+    },
+  });
 
   inject.utils.registerSlashCommand({
     name: "annoy",
@@ -75,11 +78,15 @@ export function start(): void {
     ],
     executor: (i) => {
       return {
-        result: i.getValue("message").split("").map(v => `||${v}||`).join(""),
-        send: true
-      }
-    }
-  })
+        result: i
+          .getValue("message")
+          .split("")
+          .map((v) => `||${v}||`)
+          .join(""),
+        send: true,
+      };
+    },
+  });
 }
 
 export function stop(): void {
